a0e33778d5323f4dedc5eecacf34ad23c795a79f
TacTac <110001003+KJKhalil@users.noreply.github.com>
2022-10-27 03:03:14 -0400

    Fixed not being able to useing require() by downgrading inquirer

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const Manager = require('./lib/manager.js');
 const Engineer = require('./lib/engineer.js');
 const Intern = require('./lib/intern.js');
 
+
 const Employee = {
     Manager: 'Manager',
     Engineer: 'Engineer',
